328bd158c9f84933880013d5996c1fd354ae812f
HarishkumarSP <61948549+HarishkumarSP@users.noreply.github.com>
2021-03-17 12:05:33 +0530

    course data commit

--- course-listings/src/components/Search.js
+++ course-listings/src/components/Search.js
@@ -11,30 +11,24 @@ class Search extends Component {
             courses: []
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSearch =  this.handleSearch.bind(this);
         this.getCourseDetails = this.getCourseDetails.bind(this);
+        this.handleSearch =  this.handleSearch.bind(this);
     }
 
     handleChange = (event) => {
-        this.setState(
-            {
-                searchValue: event.target.value
-            }
-        ) 
-    }
+        const searchValue = event.target.value;
+        this.setState({
+            searchValue : searchValue
+        });
+    }    
 
     getCourseDetails = async () => {
-        try {
-            let response = await axios.get('https://nut-case.s3.amazonaws.com/coursessc.json')
-            .then((response)=> console.log(response))
-            .then((data)=> console.log(data))
-            this.setState({
-                courses : response.data
-            });
-        } catch (err) {
-            console.log(err);
-        }
-   
+        let courseUrl = await axios.get('https://nut-case.s3.amazonaws.com/coursessc.json')
+        console.log(courseUrl)
+        this.setState({
+            courses : courseUrl.data
+        })
+        
     }
 
     handleSearch = () => {
@@ -42,36 +36,41 @@ class Search extends Component {
     }
 
     render(){
+        const { searchValue } = this.state;
+        console.warn(this.state);
+        const { courses } = this.state;
         return (
-            <div className="">
+            <div className="container">
             <h1 className="text-center text-primary">Welcome to course listings</h1>
-                <input className="form-control" type="text" 
-                placeholder="Search" onChange={(event) => this.handleChange(event) }
-                value= {this.state.searchValue} />
-                <button className="btn btn-outline-primary" onClick={this.handleSearch}>Search</button>
-                {this.state.courses ? (
+                <input className="form-control" type="text" name="searchValue" 
+                placeholder="Search" value={searchValue} onChange={(event)=> this.handleChange(event)}/>
+                <button className="btn btn-outline-primary" onClick={this.handleSearch}><i className="fa fa-search search-icon"/> Search</button>
+                {courses ? (
                     <div>
-                        {this.state.courses.map((course)=>{
-                            <div key={course["Course Id"]}>
+                        {courses.map((course,index) => {
+                            return (
+                            <div key={index}>
                                 <h1>Course Id:{course["Course Id"]}</h1>
                                 <div className="card card-primary">
                                     <div className="card-block">
                                         <h2 className="card-title">Course Name:{course["Course Name"]}</h2>
-                                        <label for="provider" className="card-text">Provider:{course["Provider"]}</label>
-                                        <label for="Universities/Institutions" className="card-text">Universities/Institutions:{course["Universities/Institutions"]}</label>
-                                        <label for="Parent Subject" className="card-text">Parent Subject:{course["Parent Subject"]}</label>
-                                        <label for="Child Subject" className="card-text">Child Subject:{course["Child Subject"]}</label>
+                                        <label htmlFor="provider" className="card-text">Provider:{course["Provider"]}</label>
+                                        <label htmlFor="Universities/Institutions" className="card-text">Universities/Institutions:{course["Universities/Institutions"]}</label>
+                                        <label htmlFor="Parent Subject" className="card-text">Parent Subject:{course["Parent Subject"]}</label>
+                                        <label htmlFor="Child Subject" className="card-text">Child Subject:{course["Child Subject"]}</label>
                                         <a href={course["Url"]} >Click here to dive into our course</a>
-                                        <label for="Next Session Date">Next Session Date:{course["Next Session Date"]}</label>
-                                        <label for="Length">Length:{course["Length"]}</label>
+                                        <label htmlFor="Next Session Date">Next Session Date:{course["Next Session Date"]}</label>
+                                        <label htmlFor="Length">Length:{course["Length"]}</label>
                                         <label>Video(Url):<a href={course["Video(Url)"]}>heloourl</a></label>
                                     </div>
                                 </div>
                             </div>
-                        })}
+                        )})}
                     </div>
                 ):(
-                   <p>Try Searching for a courses</p> 
+                    <div className="card text-center">
+                   <h2>Try Searching for a courses</h2> 
+                   </div>
                 )}
                 </div>
                 )}
